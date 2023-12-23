6f69e54fc2c7c5e34deb21a0f737ca3a7a55ac56
ChoiWheatley <chltmdgus604@naver.com>
2023-12-23 21:20:45 +0900

    feat(octokit-service): Add simple endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { OctokitService } from 'nestjs-octokit';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly octokitService: OctokitService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('api/pulls')
+  async pulls(): Promise<Response> {
+    const res = await this.octokitService.request(
+      'GET /repos/Jungle-7-Algorithm-study/Algorithm-Study/pulls',
+      {
+        owner: 'Jungle-7-Algorithm-study',
+        repo: 'Algorithm-Study',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      },
+    );
+    return res.data;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OctokitModule } from 'nestjs-octokit';
 
 @Module({
-  imports: [],
+  imports: [
+    OctokitModule.forRoot({
+      isGlobal: true,
+      octokitOptions: {
+        auth: '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',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
